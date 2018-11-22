9d48ec883c67fabf68705a94799f3d3daba1d367
Lucius Yu <Lucius.Yu@acxiom.com>
2018-11-22 16:34:28 +0800

    init

---
+++ springcloud-config/src/main/java/com/springcloud/config/Application.java
@@ -0,0 +1,13 @@
+package com.springcloud.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+}
